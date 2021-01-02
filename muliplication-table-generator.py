5583bdae143d8de721bb6d21b0853e7a0a7d8a47
Jay Vishwakarma <id40.jay@gmail.com>
2021-01-03 03:20:08 +0530

    Added Muliplication Table Generator

--- muliplication-table-generator.py
+++ muliplication-table-generator.py
@@ -3,5 +3,6 @@ num = int(input("Enter the number:"))
 
 # lim variable is used to store data which is used to make a limit of table
 lim = int(input("Limit of the table:"))
+
 for x in range(1, lim+1):
     print(num, '*', x, '=', num*x)
