43fa6cd6a4cbfee9443324670b6a048ce330db9d
Jay Vishwakarma <id40.jay@gmail.com>
2021-01-03 03:16:32 +0530

    Added Muliplication Table Generator

---
+++ muliplication-table-generator.py
@@ -0,0 +1,7 @@
+# num variable is used to store data which is used to make a table
+num = int(input("Enter the number:"))
+
+# lim variable is used to store data which is used to make a limit of table
+lim = int(input("Limit of the table:"))
+for x in range(1, lim+1):
+    print(num, '*', x, '=', num*x)
